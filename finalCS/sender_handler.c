2cd35d29f5de19d7ad51631c6ec5764a43544240
Nate Chan <nkc53@nau.edu>
2023-04-19 22:41:21 -0700

    worked on send_handler files

--- finalCS/sender_handler.c
+++ finalCS/sender_handler.c
@@ -10,7 +10,8 @@
 // Handles how messages are sent to the chat
 void *sender_handler(void *arg) {
     // Cast the argument to the ChatNode linked list struct
-
+    ChatNodeType *server = (ChatNodeType *) arg;
+    message *message = (ChatNodeType *) arg;
     // main loop for the sender thread,
     // which waits for user input and calls the appropriate
     // functions to handle the user's requests.
@@ -40,12 +41,48 @@ int send_join_request(ChatNode *server) {
     // This function should send a message to the
     // server that includes the client's IP address, port number,
     // and logical name.
+
+    // create request msg to send to server
+    message msg;
+
+    // set msg type to join
+    msg.type = JOIN;
+    
+    // set note attribs
+    sprintf(msg.note, "%s:%d:%s", server->IP, server->port, server->Name);
+
+    // send mssg
+    send_message(msg, server->sockfd )
+
+    return 0
 }
 
 // Sends a LEAVE request to the server
 int send_leave_request(ChatNode *server) {
     // This function should send a message to the
     // server that includes the client's logical name.
+
+    // create mssg n set to type Leave
+    message msg;
+    msg.type = LEAVE;
+
+    // set sender field to current client name
+    strncpy(msg.sender, server->name, MAX_NAME_LEN);
+
+    // convert msg struct to a byte stream
+    buffer[sizeof(msg)];
+    memcpy(buffer, &msg, sizeof(msg));
+
+    // send message stream to socket
+    int n = write(server->sockfd, buffer, sizeof(msg));
+    if (n < 0) {
+        perror("Error writing to socket");
+        exit(1);
+    }
+
+    // close socket and exit thread?
+    close(server->sockfd);
+    pthread_exit(NULL);
 }
 
 // Sends a message to the server
